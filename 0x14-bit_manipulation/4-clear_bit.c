3e27bb3ea262e4a8946eb17ddace6e81f3a73c88
Adeola200 <adetorodeborah191@gmail.com>
2023-06-08 06:53:30 -0700

    4-clear_bit.c

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -1,8 +1,8 @@
 #include "main.h"
 
 /**
- * clear_bit - sets the value of a giben bit to 0
- * @n: pointer to the to change
+ * clear_bit - sets the value of a given bit to 0
+ * @n: pointer to the number to change
  * @index: index of the bit to clear
  *
  * Return: 1 for success, -1 for failure
